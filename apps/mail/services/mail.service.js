4b92aeb36960649a5af4877f45350c56d8bf6964
Moria Hamami <moriahamami@gmail.com>
2022-12-28 13:27:33 +0200

    Merged async storage and storage services

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,26 @@
-console.log('Hi')
+import { utilService } from '../../../services/util.service'
+import { storageService } from '../../../services/storage.service'
+import { asyncStorageService } from '../../../services/async-storage.service'
+
+const MAIL_KEY = 'mailDB'
+
+_createLoggedUser
+_createMails
+
+function _createLoggedUser() {
+    const loggedinUser = {
+        email: 'user@appsus.com',
+        fullname: 'Mahatma Appsus'
+    }
+}
+
+function _createMails() {
+    const email = {
+        id: 'e101',
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false,
+        sentAt: 1551133930594,
+        to: 'momo@momo.com'
+    }
+}
