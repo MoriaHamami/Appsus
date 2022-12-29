3447f371367b4e04d8263eb6d6a5b8553f2b52b3
Yael tal <yaelital27@gmail.com>
2022-12-29 23:30:53 +0200

    updating home page and header

--- views/home.jsx
+++ views/home.jsx
@@ -3,12 +3,31 @@ const { Link } = ReactRouterDOM
 export function Home() {
 
     return <section className="home">
-        <h1>Welcome to Appsus</h1>
+        <h5 className="animate__animated">Welcome to</h5>
+        <h1 className="animate__animated">Appsus</h1>
+        <div className="apps-container animate__animated">
+            <Link to="/mail">
+                <div className="container mail">
+                    <img src="./assets/img/icons/icons-mail/icons8-gmail-480.svg" alt="email" />
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, praesentium ducimus incidunt veniam sed deserunt commodi! Ipsum autem impedit totam quod earum incidunt repellendus libero saepe quibusdam tempore? Ab, veritatis?</p> */}
+                </div>
+            </Link>
+            <Link to="/note">
+                <div className="container note">
+                    <img src="./assets/img/icons/icons-notes/icons8-google-keep-480.svg" alt="note" />
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, praesentium ducimus incidunt veniam sed deserunt commodi! Ipsum autem impedit totam quod earum incidunt repellendus libero saepe quibusdam tempore? Ab, veritatis?</p> */}
+                </div>
+            </Link>
 
-        <Link to="/mail"><img className="mail" src="./assets/img/icons/icons-mail/miss-mail-logo.png" alt="email" /></Link>
-        <Link to="/note"><img className="note" src="./assets/img/icons/icons-notes/note-logo.png" alt="note" /></Link>
-        <Link to="/book"><img className="book" src="./assets/img/icons/icons-book/book-logo.png" alt="book" /></Link>
+            <Link to="/book">
+                <div className="container book ">
+                    <img src="./assets/img/icons/icons-book/icons8-google-books-480.svg" alt="book" />
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, praesentium ducimus incidunt veniam sed deserunt commodi! Ipsum autem impedit totam quod earum incidunt repellendus libero saepe quibusdam tempore? Ab, veritatis?</p> */}
+                </div>
+            </Link>
 
+
+        </div>
 
     </section>
 }
