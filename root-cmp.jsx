269e5a1d17f15e669a49d9e9359d675aeb43e41e
Moria Hamami <moriahamami@gmail.com>
2022-12-31 20:57:37 +0200

    Cleaning code

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,19 +1,19 @@
 const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
+// Base imports
+import { Home } from "./views/home.jsx"
 import { AppHeader } from "./cmps/app-header.jsx"
 import { About } from "./views/about.jsx"
-import { Home } from "./views/home.jsx"
+import { UserMsg } from './cmps/user-msg.jsx'
+// Mail imports
 import { MailIndex } from "./apps/mail/views/mail-index.jsx"
+// Note imports
 import { NoteIndex } from "./apps/note/views/note-index.jsx"
-import { NoteDetails } from "./apps/note/cmps/note-details.jsx"
-// import { AppHeader } from "./cmps/app-header.jsx"
-
+// Book imports
 import { BookIndex } from './apps/book/views/book-index.jsx'
 import { BookDetails } from './apps/book/views/book-details.jsx'
 import { BookEdit } from './apps/book/views/book-edit.jsx'
-import { UserMsg } from './cmps/user-msg.jsx'
-import { AboutTeam } from "./cmps/about-team.jsx"
 
 export function App() {
 
@@ -25,47 +25,35 @@ export function App() {
         document.body.classList.toggle('mail-menu-open')
     }
 
-
     return <Router>
         <section className="app">
 
             <div className="main-screen" onClick={toggleMenu}></div>
             <div className="mail-screen" onClick={toggleMailMenu}></div>
+            
             <AppHeader />
+
             <Routes>
+
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                    {/* <Route element={<AboutTeam />} path="/about/team" /> */}
-                {/* </Route> */}
 
                 <Route path="/mail" element={<MailIndex />} />
                 <Route path="/mail:info" element={<MailIndex />} />
+
                 <Route path="/note" element={<NoteIndex />} />
                 <Route path="/note:info" element={<NoteIndex />} />
-                {/* <Route path="/note/:noteId" element={<NoteDetails />} /> */}
 
-                {/* <Route path="/book" element={<BookIndex />} /> */}
-                
-                {/* <Router>
-                    <section className="main-layout app">
-
-                        <AppHeader /> */}
-
-                            {/* <Routes> */}
-                                {/* <Route element={<HomePage />} path="/" /> */}
-                                    {/* <Route element={<AboutUs />} path="/aboutUs" /> */}
-                                    <Route element={<BookIndex />} path="/book" />
-                                    <Route element={<BookEdit />} path="/book/edit" />
-                                    <Route element={<BookEdit />} path="/book/edit/:bookId" />
-                                    <Route element={<BookDetails />} path="/book/:bookId" />
-                            {/* </Routes> */}
-
-                    {/* </section>
-                </Router> */}
+                <Route path="/book" element={<BookIndex />}  />
+                <Route path="/book/edit" element={<BookEdit />} />
+                <Route path="/book/edit/:bookId" element={<BookEdit />} />
+                <Route path="/book/:bookId" element={<BookDetails />} />
 
 
             </Routes>
-                </section>
-                <UserMsg />
+        </section>t
+        
+        <UserMsg />
+
     </Router>
 }
