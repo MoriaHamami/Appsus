68aa1e96f5b26a57b553afbc57014a7d563a9031
Yael tal <yaelital27@gmail.com>
2022-12-31 21:00:00 +0200

    submission - 21:00

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -8,32 +8,6 @@ export function NoteList({ notes, onRemoveNote , onSaveNote}) {
 
     const [isPinned, setIsPinned] = useState(false)
 
-    function noteIsPinned() {
-        if (isPinned) {
-            return <div>
-                {/* <h1>Pinned</h1> */}
-                {notes.map(note => {
-                    console.log('pinned', note);
-                    {
-                        if (note.isPinned) {
-                           return <NotePreview note={note} isPinned={note.isPinned} onRemoveNote={onRemoveNote}/>
-                        }
-                    }
-                })}
-
-                <h1>Others</h1>
-                {notes.map(note => {
-                   {return !note.isPinned && <NotePreview note={note} isPinned={note.isPinned} onRemoveNote={onRemoveNote}/>}
-                })}
-
-            </div>
-        } else {
-            return <div>
-                <h1></h1>
-                <NotePreview />
-            </div>
-        }
-    }
 
     return <ul className="note-list">
         {
@@ -41,17 +15,6 @@ export function NoteList({ notes, onRemoveNote , onSaveNote}) {
 
                 <NotePreview note={note} isPinned={note.isPinned} onRemoveNote={onRemoveNote} onSaveNote={onSaveNote}/>
                 
-                {/* {noteIsPinned()} */}
-                {/* {note.isPinned && <NotePreview note={note} isPinned={note.isPinned}/>} */}
-
-                {/* <h1>Pinned</h1>
-                {isPinned && <NotePreview note={note} />}
-                <h1>Rest</h1>
-                {!isPinned && <NotePreview note={note} />} */}
-
-                {/* <Link to={`/note/${note.id}`}><NotePreview note={note} /></Link> */}
-                {/* <button onClick={() => onRemoveNote(note.id)}>x</button> */}
-
                 
             </li>)
         }
--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -17,13 +17,7 @@ export function NotePreview({ note, onRemoveNote, onSaveNote, onEditNote }) {
     const [isShown, setIsShown] = useState(false)
     const [color, setColor] = useState(noteService.get(note.id))
     const [content, setContent] = useState(null)
-    // const [content, setContent] = useState(noteService.get(note.id))
-    // console.log('color', color);
-    // console.log('isPinned', isPinned);
-    // const imgName = book.name ? book.name : 'default'
-    // <img src={`assets/img/${imgName}.png`} />
 
-    // console.log('content', content);
     const colorRef = useRef(null)
     const contentRef = useRef(null)
 
@@ -85,42 +79,23 @@ export function NotePreview({ note, onRemoveNote, onSaveNote, onEditNote }) {
 
     }
 
-    // function onEditNote() {
-    //     if (contentRef.current.contentEditable) {
-    //         console.log('true')
-    //         contentRef.current.contentEditable = true
-    //         contentRef.current.focus()
-    //         contentRef.current = innerText
-    //         changeContent()
-    //     } else {
-    //         console.log('false')
-    //         contentRef.current.contentEditable = false
-    //         // contentRef.current.blur()
-    //         noteService.save(note).then((content) => {
-    //             console.log('content saved', content);
+  
 
-
-    //         })
-    //     }
-    // }
-
-
-    // return <article onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} ref={colorRef} className="note-preview" style={{ backgroundColor: note.style.backgroundColor }}>
+   
     return <article ref={colorRef} className="note-preview" style={{ backgroundColor: note.style.backgroundColor }}>
 
 
-        {/* {<div ref={contentRef} onKeyDown={(ev) => changeContent(ev)} contentEditable={false} suppressContentEditableWarning={true} > */}
         {<div  >
             {noteType()}
 
         </div>}
 
 
-        {/* <Link to={`/note/${note.id}`}>select</Link> */}
 
 
 
-        {/* {isShown && ( */}
+
+
         <div className="hidden-buttons">
             <form className="tooltip" onChange={changeColor}>
                 <label htmlFor={`color-${note.id}`}><img src="./assets/img/icons/icons-notes/asset 22.svg" alt="" /><span className="tooltiptext">Color</span></label>
@@ -137,12 +112,11 @@ export function NotePreview({ note, onRemoveNote, onSaveNote, onEditNote }) {
            {<NoteToMail note={note} />}
             
             <button className="tooltip" onClick={() => onRemoveNote(note.id)}><img src="./assets/img/icons/icons-notes/delete_FILL0_wght400_GRAD0_opsz48.svg" alt=""/><span className="tooltiptext">Delete</span></button>
-            {/* { <button className="edit_content tooltip" onClick={onEditNote(note.id)}>edit<span className="tooltiptext">Edit</span></button>} */}
-            {/* { <button className="edit_content tooltip" onClick={(ev) => onEditNote(ev, note.id)}>edit<span className="tooltiptext">Edit</span></button>} */}
-        </div>
-        {/* )}  */}
 
-        {/* <NoteDetails note={note} /> */}
+        </div>
+
+
+
 
     </article>
 }
